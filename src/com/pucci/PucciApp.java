86f353c50c75813b761a5dee26b21e138a0cf6e5
Kaelan <kadokaelan@gmail.com>
2018-08-02 13:24:17 -0400

    First Pucci init

---
+++ src/com/pucci/PucciApp.java
@@ -0,0 +1,42 @@
+package com.pucci;
+
+public class PucciApp {
+
+	public static void main(String[] args) {
+		// TODO Welcome User to Pucci and give instruction
+		
+		
+		
+		// TODO Display menu ( Men (tops, bottoms, shoes)      |        Womens(tops, bottoms, shoes) )
+		
+		
+		
+		// TODO Get input from user  ---  read product text file ---- return list
+		
+		
+		
+		// TODO Add to cart? (do - while loop to product menu) (write to cart text file)
+		
+		
+		
+		// TODO loop back when user done shopping
+		
+		
+		
+		// TODO print out user cart (read cart text file)
+		
+		
+		
+		// TODO prompt user for payment (cash, check, charge) (Validator class)
+		
+		
+		
+		// TODO display reciept (items ordered, subtotal, grand total, appropriate payment info)
+		
+		
+		// TODO goodbye have a pucci day
+		
+		
+	}
+
+}
