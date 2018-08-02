6c818e459582df367df2701ccd98afd64c508516
Brandon Spencer <Owner@MacBook-Air.local>
2018-08-02 17:53:06 -0400

    added Bottoms and Shoes

---
+++ src/com/pucci/Bottoms.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.pucci;
+
+
+
+/**
+ * @author Brandon Spencer
+ *
+ */
+public class Bottoms extends Clothes {
+
+	public Bottoms(String name, String type, String price, String inventory) {
+		super(name, type, price, inventory);
+	}
+
+	
+}
---
+++ src/com/pucci/Shoes.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.pucci;
+
+
+/**
+ * @author Brandon Spencer
+ *
+ */
+public class Shoes extends Clothes {
+
+	public Shoes(String name, String type, double price, int inventory, String size) {
+		super(name, type, price, inventory);
+		
+	}
+	
+	
+}
