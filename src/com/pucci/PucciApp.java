b2e3ddc42b1b74ab56edd7700f4d83cd909344ae
Kaelan <kadokaelan@gmail.com>
2018-08-02 14:35:15 -0400

    Added a few classes

--- src/com/pucci/PucciApp.java
+++ src/com/pucci/PucciApp.java
@@ -1,17 +1,35 @@
 package com.pucci;
 
+import java.util.Scanner;
+
 public class PucciApp {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		// All variable declarations
+		int userInput;
+		
+		
+		
 		// TODO Welcome User to Pucci and give instruction
 		
+		System.out.println("Welcome to Pucci");
+		System.out.println("select a category from the menu below");
 		
+		
+		
+	
 		
 		// TODO Display menu ( Men (tops, bottoms, shoes)      |        Womens(tops, bottoms, shoes) )
 		
 		
+		printFirstMenu();
+		
 		
 		// TODO Get input from user  ---  read product text file ---- return list
+		
+		userInput = Validator.getInt(scan, "Please select the number of which category you would like", 1 , 7);
 		
 		
 		
@@ -31,12 +49,21 @@ public class PucciApp {
 		
 		
 		
-		// TODO display reciept (items ordered, subtotal, grand total, appropriate payment info)
+		// TODO display receipt (items ordered, subtotal, grand total, appropriate payment info)
 		
 		
-		// TODO goodbye have a pucci day
 		
+		// TODO goodbye
+		System.out.println("Have a Pucci day");
 		
+	}
+
+	public static void printFirstMenu() {
+		String[] firstMenu = { "Men \t\t Women", "1.Top \t\t 4.Top", "2.Bottoms \t 5.Bottoms",
+				"3.Shoes \t 6.Shoes", "\t 7.Exit" };
+		for (int i = 0; i < firstMenu.length; i++) {
+			System.out.println(firstMenu[i]);
+		}
 	}
 
 }
