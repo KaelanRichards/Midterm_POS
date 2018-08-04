d380fe3af6e5b3a59648801f36c26fa546df51e1
Brandon Spencer <Owner@Brandons-MacBook-Air.local>
2018-08-04 13:30:28 -0400

    comments added to shoes and bottoms methods, tostring methods commented out

--- src/com/pucci/Bottoms.java
+++ src/com/pucci/Bottoms.java
@@ -16,6 +16,7 @@ public class Bottoms extends Clothes {
 
 	}
 
+	// this method will print the list of men's bottoms in a numbered format
 	public static void printMenBottoms() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFileBottoms("Products.txt");
 		ArrayList<Clothes> mBottomList = new ArrayList<>();
@@ -29,6 +30,7 @@ public class Bottoms extends Clothes {
 		}
 	}
 
+	// this method will print the list of women's bottoms in a numbered format
 	public static void printWomenBottoms() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFileBottoms("Products.txt");
 		ArrayList<Clothes> wBottomList = new ArrayList<>();
@@ -43,6 +45,7 @@ public class Bottoms extends Clothes {
 		}
 	}
 
+	// this method returns the arraylist of men's bottoms
 	public static ArrayList<Clothes> getMBottomList() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFile("Products.txt");
 		ArrayList<Clothes> mBottomList = new ArrayList<>();
@@ -52,6 +55,7 @@ public class Bottoms extends Clothes {
 		return mBottomList;
 	}
 
+	// this method returns the arraylist of women's bottoms
 	public static ArrayList<Clothes> getWBottomList() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFile("Products.txt");
 		ArrayList<Clothes> wBottomList = new ArrayList<>();
@@ -61,12 +65,17 @@ public class Bottoms extends Clothes {
 		return wBottomList;
 	}
 	
+	// this method adds men's bottoms to the shopping cart by using an enhanced for loop to iterate over the
+	// Products.txt file, referencing the index of the item.
 	public static void getMenBottomsToCart(int userChoice) {
 
 		int i = userChoice;
 		for (i = 7; i <= 8; i++) {
 			if ((i - 6) == userChoice) {
-
+				// (i - 6) is used in the if statement because when the list of men's bottoms is displayed to the user,
+				// it is shown in a numbered list beginning with 1. So when the user enters their selection (1 or 2),
+				// 6 is added to userChoice and the index at that location is added to the Shopping Cart, 
+				// i.e. if a user enters 1, index 7 from the Products.txt file will be added to the cart.
 				ShoppingCart.addItem(FileMethods.readFromFileBottoms("Products.txt").get(i));
 			}
 		}
@@ -77,16 +86,19 @@ public class Bottoms extends Clothes {
 		int i = userChoice;
 		for (i = 12; i <= 13; i++) {
 			if ((i - 11) == userChoice) {
-
+				// (i - 11) is used in the if statement because when the list of women's bottoms is displayed to the user,
+				// it is shown in a numbered list beginning with 1. So when the user enters their selection (1 or 2),
+				// 11 is added to userChoice and the index at that location is added to the Shopping Cart, 
+				// i.e. if a user enters 1, index 12 from the Products.txt file will be added to the cart.
 				ShoppingCart.addItem(FileMethods.readFromFileBottoms("Products.txt").get(i));
 			}
 		}
 	}
 
-	@Override
-	public String toString() {
-
-		return String.format("%-18s  %-18s", getName(), (getPrice()));
-		
+//	@Override
+//	public String toString() {
+//
+//		return String.format("%-18s  %-18s", getName(), (getPrice()));
+//		
 	}
-}
+
--- src/com/pucci/Shoes.java
+++ src/com/pucci/Shoes.java
@@ -15,6 +15,7 @@ public class Shoes extends Clothes {
 		super(gender, name, type, price, inventory);
 	}
 
+	// this method prints the list of men's shoes in a numbered list.
 	public static void printMenShoes() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFileShoes("Products.txt");
 		ArrayList<Clothes> mShoeList = new ArrayList<>();
@@ -29,6 +30,7 @@ public class Shoes extends Clothes {
 		}
 	}
 
+	// this method prints the list of women's shoes in a numbered list.
 	public static void printWomenShoes() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFileShoes("Products.txt");
 		ArrayList<Clothes> wShoeList = new ArrayList<>();
@@ -43,6 +45,7 @@ public class Shoes extends Clothes {
 		}
 	}
 
+	// this method returns the arraylist of men's shoes.
 	public static ArrayList<Clothes> getMShoeList() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFile("Products.txt");
 		ArrayList<Clothes> mShoeList = new ArrayList<>();
@@ -52,7 +55,7 @@ public class Shoes extends Clothes {
 		return mShoeList;
 
 	}
-
+	// this method returns the arraylist of women's shoes.
 	public static ArrayList<Clothes> getWShoeList() {
 		ArrayList<Clothes> menu2 = FileMethods.readFromFile("Products.txt");
 		ArrayList<Clothes> wShoeList = new ArrayList<>();
@@ -65,11 +68,13 @@ public class Shoes extends Clothes {
 	public static void getMenShoesToCart(int userChoice) {
 
 		int i = userChoice;
-		ArrayList<Clothes> arr = new ArrayList<>();
 		
 		for (i = 5; i <= 6; i++) {
 			if ((i - 4) == userChoice) {
-				
+				// (i - 4) is used in the if statement because when the list of men's shoes is displayed to the user,
+				// it is shown in a numbered list beginning with 1. So when the user enters their selection (1 or 2),
+				// 4 is added to userChoice and the index at that location is added to the Shopping Cart, 
+				// i.e. if a user enters 1, index 5 from the Products.txt file will be added to the cart.
 				ShoppingCart.addItem(FileMethods.readFromFileShoes("Products.txt").get(i));
 			}
 		}
@@ -80,14 +85,17 @@ public class Shoes extends Clothes {
 		int i = userChoice;
 		for (i = 14; i <= 15; i++) {
 			if ((i - 13) == userChoice) {
-
+				// (i - 13) is used in the if statement because when the list of men's bottoms is displayed to the user,
+				// it is shown in a numbered list beginning with 1. So when the user enters their selection (1 or 2),
+				// 13 is added to userChoice and the index at that location is added to the Shopping Cart, 
+				// i.e. if a user enters 1, index 14 from the Products.txt file will be added to the cart.
 				ShoppingCart.addItem(FileMethods.readFromFileShoes("Products.txt").get(i));
 			}
 		}
 	}
 
-	@Override
-	public String toString() {
-		return String.format("%-18s %-18s", getName(), (getPrice()));
+//	@Override
+//	public String toString() {
+//		return String.format("%-18s %-18s", getName(), (getPrice()));
 	}
-}
+
